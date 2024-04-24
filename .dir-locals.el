0451100e33a6c36f419dfcd948b9917f68d19195
Bastien Riviere <me@babariviere.com>
2024-04-24 15:52:41 +0200

    feat: add combos

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,5 @@
-((auto-mode-alist . (("\\.keymap\\'" . c-mode))))
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((auto-mode-alist . (("\\.keymap\\'" . c-mode)))
+ (c-mode . ((tab-width . 4))))
