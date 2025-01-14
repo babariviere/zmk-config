1bec330db7c16677c324b0d8633552a8231144e9
Bastien Rivière <me@babariviere.com>
2025-01-14 17:57:52 +0100

    Delete .dir-locals.el

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((auto-mode-alist . (("\\.keymap\\'" . c-mode)))
- (c-mode . ((tab-width . 4))))
